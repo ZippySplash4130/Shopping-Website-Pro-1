2b1db0a35a42e3a25033ea21502dba55ceeef2ee
ZippySplash4130 <ShehrozIlyas615@Gmail.com>
2025-01-01 14:01:46 +0000

    Added new models with Product and Order. The website is currently being designed for searcg bar and product to be made

--- Shopping/shop/models.py
+++ Shopping/shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(decimal_places=2, max_digits=2)
+    stock = models.IntegerField()
+    available = models.BooleanField()
+    product_id = models.IntegerField()
+    
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
+
--- Shopping/shop/views.py
+++ Shopping/shop/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def first_website(requests):
     return render(requests, 'rawr.html')
+
+def search(requests):
+    query = requests.GET.get("q")
+    results = []
+    pass
